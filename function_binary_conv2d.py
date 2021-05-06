2f61841db1ded41e2b8ba278ce1f094a105fe3a1
kanamori-takuto-0916 <kanamori.takuto.ae@gmail.com>
2021-05-06 16:40:25 +0900

    v2

--- function_binary_conv2d.py
+++ function_binary_conv2d.py
@@ -21,7 +21,7 @@ def _as_mat(x):
 if cuda.cudnn_enabled:
     cudnn = cuda.cudnn
     #libcudnn = cuda.cudnn.cudnn
-    libcudnn = cuda.cudnn.cudnn
+    libcudnn = cuda.cudnn
     _cudnn_version = libcudnn.getVersion()
     _fwd_pref = libcudnn.CUDNN_CONVOLUTION_FWD_SPECIFY_WORKSPACE_LIMIT
     if _cudnn_version >= 4000:
