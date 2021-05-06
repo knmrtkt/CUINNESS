4d9f4a769ae3a7e24ae33bf2812df4376ae523f4
kanamori-takuto-0916 <kanamori.takuto.ae@gmail.com>
2021-05-06 17:08:50 +0900

    v2

--- function_binary_linear.py
+++ function_binary_linear.py
@@ -6,11 +6,15 @@ from chainer import function
 from chainer.utils import type_check
 
 def _kern():
-    return cuda.elementwise(
-    return cupy.ElementwiseKernel(
+    # return cuda.elementwise(
+    #     'T x', 'T y',
+    #     'y = x >= 0 ? 1 : -1',
+    #     'binarize')
+    ret = cupy.ElementwiseKernel(
         'T x', 'T y',
         'y = x >= 0 ? 1 : -1',
         'binarize')
+    return ret
 
 def _as_mat(x):
     if x.ndim == 2:
